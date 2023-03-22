fe381735aa445d82f685076cdf4fa61dccd81b95
Ran <ran.wahle@gmail.com>
2023-03-22 09:43:19 +0200

    Typed callback

--- src/hooks/useTranslation.tsx
+++ src/hooks/useTranslation.tsx
@@ -1,16 +1,16 @@
-import i18n, { InitOptions as InitOptionsProps } from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import resourcesToBackend from 'i18next-resources-to-backend';
+import i18n, { InitOptions as InitOptionsProps } from "i18next";
+import { initReactI18next } from "react-i18next";
+import resourcesToBackend from "i18next-resources-to-backend";
 
-export * from 'react-i18next';
+export * from "react-i18next";
 
 export const i18nInstance = i18n.createInstance();
 
 const initOptions: InitOptionsProps = {
-  lng: 'en',
-  ns: ['main'],
-  defaultNS: 'main',
-  fallbackLng: 'en',
+  lng: "en",
+  ns: ["main"],
+  defaultNS: "main",
+  fallbackLng: "en",
   saveMissing: true,
   react: {
     useSuspense: true,
@@ -20,24 +20,27 @@ const initOptions: InitOptionsProps = {
   },
 };
 
+type LocaleImportCallback = (arg: any, resources: any) => void;
+
 i18nInstance
   .use(
-      resourcesToBackend((language: string, namespace: string, callback
-          : (arg: any, resources: any) => Promise<any>) => {
-      // Just import from local.
-      // using https://www.i18next.com/how-to/backend-fallback#you-can-also-lazy-load-the-in-memory-translations-i.e.-when-using-webpack
-      import(`../assets/locales/${language}/${namespace}.json`)
-        .then((resources) => {
-          callback(null, resources);
-        })
-        .catch((error) => callback(error, null));
-    })
+    resourcesToBackend(
+      (language: string, namespace: string, callback: LocaleImportCallback) => {
+        // Just import from local.
+        // using https://www.i18next.com/how-to/backend-fallback#you-can-also-lazy-load-the-in-memory-translations-i.e.-when-using-webpack
+        import(`../assets/locales/${language}/${namespace}.json`)
+          .then((resources) => {
+            callback(null, resources);
+          })
+          .catch((error) => callback(error, null));
+      }
+    )
   )
   .use(initReactI18next)
   .init(initOptions, (error) => {
     if (error) {
       // eslint-disable-next-line no-console
-      console.log('Error occured when initialize the i18next: ', error);
+      console.log("Error occured when initialize the i18next: ", error);
     }
     return true;
   });
