83106b89592ef78ae7af140f9423f0b66b267683
Ran <ran.wahle@gmail.com>
2023-03-22 10:15:54 +0200

    reorganazie styled navbar

--- src/components/vavbar/navbar.tsx
+++ src/components/vavbar/navbar.tsx
@@ -1,28 +1,26 @@
-import { useTranslation } from '../../hooks/useTranslation';
-import { StyledNavbar } from './navbar.styled';
+import { useTranslation } from "../../hooks/useTranslation";
+import StyledNavbar from "./navbar.styled";
 export const Navbar = () => {
-    const { t } = useTranslation();
-    return <>
-        <StyledNavbar.Container>
-            <StyledNavbar.NavbarBrand>
-                <a href="/#/">{ t('header.companyName')}</a>
-            </StyledNavbar.NavbarBrand>
-            <StyledNavbar.RightMenu>
-                <StyledNavbar.NavItem>
-                    <a href='/#/'>{ t('navigationMenu.home')}</a>
-                </StyledNavbar.NavItem>
-                <StyledNavbar.NavItem>
-                    <a>
-                        { t('navigationMenu.signIn')}
-                    </a>
-                </StyledNavbar.NavItem>
-                <StyledNavbar.NavItem>
-                    <a>
-                        {t('navigationMenu.signUp')}
-                    </a>
-                </StyledNavbar.NavItem>
-            </StyledNavbar.RightMenu>
-            <StyledNavbar.End></StyledNavbar.End>
-        </StyledNavbar.Container>
-    </>;
-}
+  const { t } = useTranslation();
+  return (
+    <>
+      <StyledNavbar.Container>
+        <StyledNavbar.NavbarBrand>
+          <a href="/#/">{t("header.companyName")}</a>
+        </StyledNavbar.NavbarBrand>
+        <StyledNavbar.RightMenu>
+          <StyledNavbar.NavItem>
+            <a href="/#/">{t("navigationMenu.home")}</a>
+          </StyledNavbar.NavItem>
+          <StyledNavbar.NavItem>
+            <a>{t("navigationMenu.signIn")}</a>
+          </StyledNavbar.NavItem>
+          <StyledNavbar.NavItem>
+            <a>{t("navigationMenu.signUp")}</a>
+          </StyledNavbar.NavItem>
+        </StyledNavbar.RightMenu>
+        <StyledNavbar.End></StyledNavbar.End>
+      </StyledNavbar.Container>
+    </>
+  );
+};
